51ab34e978146773b6696534a8d5018c43899901
Ishiura Hirohisa <ishiura@live.jp>
2012-04-11 03:28:18 +0900

    removed and moved.

--- fetch-sc6.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-
-cobbler import --path=rsync://ftp.jaist.ac.jp/pub/Linux/scientific/6.1/x86_64/os/ --name sl6-x86_64 --arch=x86_64
-
--- install-rpm-fromEPEL.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-rpm -Uhv http://download.fedora.redhat.com/pub/epel/6/x86_64/epel-release-6-5.noarch.rpm
-yes | yum install -y cobbler dhcp bind caching-nameserver syslinux
-
-
