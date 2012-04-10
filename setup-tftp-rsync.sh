705418d1b796ca5b0317d7ece517aa9902330f1a
Ishiura Hirohisa <ishiura@live.jp>
2012-04-11 03:29:04 +0900

    removed.

--- setup-tftp-rsync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-
-perl -i.bk -pe 's|.*disable.*|\tdisable\t\t\t= no|' /etc/xinetd.d/{tftp,rsync}
-for i in /etc/xinetd.d/{tftp,rsync};do diff $i{.bk,};done
-rm -f /etc/xinetd.d/{tftp,rsync}.bk
-service xinetd start && chkconfig xinetd on
